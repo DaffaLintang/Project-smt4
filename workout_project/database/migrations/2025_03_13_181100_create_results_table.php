c297267cb03e2886ddcf4e99ccf1401c52c4bec7
Daffa Lintang <daffalintang@gmail.com>
2025-03-15 00:06:53 +0700

    create tabel result & relationship

---
+++ workout_project/database/migrations/2025_03_13_181100_create_results_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('Results', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('desc');
+            $table->string('type');
+            $table->string('bodyPart');
+            $table->string('equipment');
+            $table->string('level');
+            $table->unsignedBigInteger('id_user');
+            $table->timestamps();
+
+            $table->foreign('id_user')->references('id')->on('user')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('Results');
+    }
+};
