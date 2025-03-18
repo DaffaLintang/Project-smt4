ac9a371aadb7062135ca7b561c0c6e11d174aae6
Annisanzn <annisanurul1724@gmail.com>
2025-03-19 00:26:47 +0700

    test progres

--- workout_project/routes/web.php
+++ workout_project/routes/web.php
@@ -8,3 +8,19 @@ Route::get('/', function () {
 });
 
 Route::get('/workout', [WorkoutController::class, 'index']);
+
+Route::get('/home', [App\Http\Controllers\DashboardController::class, 'index'])->middleware('verified')->name('dashboard');
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
+
+use App\Http\Controllers\DashboardController;
+
+Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+
+Route::middleware([])->group(function () {
+    Route::get('/admin/dashboard', [DashboardController::class, 'index']);
+});
+
+
