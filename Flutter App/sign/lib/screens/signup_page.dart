3d495e5a0a72be9ea7424d63686a57cf10287834
Daffa Lintang <daffalintang@gmail.com>
2025-05-16 00:08:32 +0700

    add back to login in sign up

--- Flutter App/sign/lib/screens/signup_page.dart
+++ Flutter App/sign/lib/screens/signup_page.dart
@@ -1,98 +1,108 @@
-  import 'package:flutter/material.dart';
-  import 'package:sign/controllers/signup_controller.dart';
-  import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:sign/controllers/signup_controller.dart';
+import 'package:get/get.dart';
 
-  class SignUpPage extends StatelessWidget {
-    final SignupController controller = Get.put(SignupController());
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 50), // Menggeser tampilan ke atas
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start, // Mengatur agar elemen lebih ke atas
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                // Logo
-                Image.asset(
-                  'assets/image/logo.png',
-                  height: 180, // Ukuran sedikit dikurangi
+class SignUpPage extends StatelessWidget {
+  final SignupController controller = Get.put(SignupController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: 30.0, vertical: 50), // Menggeser tampilan ke atas
+          child: Column(
+            mainAxisAlignment:
+                MainAxisAlignment.start, // Mengatur agar elemen lebih ke atas
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Logo
+              Image.asset(
+                'assets/image/logo.png',
+                height: 180, // Ukuran sedikit dikurangi
+              ),
+              SizedBox(height: 20), // Kurangi jarak agar lebih ke atas
+
+              // "Create Account"
+              Text(
+                'Create Account',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w900, // Extra bold
                 ),
-                SizedBox(height: 20), // Kurangi jarak agar lebih ke atas
-
-                // "Create Account"
-                Text(
-                  'Create Account',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w900, // Extra bold
+              ),
+              SizedBox(height: 15),
+              // Input Email
+              SizedBox(
+                width: 350,
+                child: TextField(
+                  controller: SignupController.emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: UnderlineInputBorder(),
                   ),
                 ),
-                SizedBox(height: 15),
+              ),
+              SizedBox(height: 15),
 
-                // Input Email
-                SizedBox(
-                  width: 350,
-                  child: TextField(
-                    controller: SignupController.emailController,
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      border: UnderlineInputBorder(),
-                    ),
+              // Input Password
+              SizedBox(
+                width: 350,
+                child: TextField(
+                  controller: SignupController.passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: UnderlineInputBorder(),
                   ),
                 ),
-                SizedBox(height: 15),
+              ),
+              SizedBox(height: 15),
 
-                // Input Password
-                SizedBox(
-                  width: 350,
-                  child: TextField(
-                    controller: SignupController.passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      border: UnderlineInputBorder(),
-                    ),
+              // Input Konfirmasi Password
+              SizedBox(
+                width: 350,
+                child: TextField(
+                  controller: SignupController.passwordConfirmController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Confirmation Password',
+                    border: UnderlineInputBorder(),
                   ),
                 ),
-                SizedBox(height: 15),
+              ),
+              SizedBox(height: 30),
 
-                // Input Konfirmasi Password
-                SizedBox(
-                  width: 350,
-                  child: TextField(
-                    controller: SignupController.passwordConfirmController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Confirmation Password',
-                      border: UnderlineInputBorder(),
-                    ),
+              // Tombol Sign Up
+              ElevatedButton(
+                onPressed: () {
+                  controller.register();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red[900],
+                  minimumSize: Size(300, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
                   ),
                 ),
-                SizedBox(height: 30),
-
-                // Tombol Sign Up
-                ElevatedButton(
-                  onPressed: () {
-                    controller.register();
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red[900],
-                    minimumSize: Size(300, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-                  child: Text(
-                    'SIGN UP',
-                    style: TextStyle(color: Colors.white),
-                  ),
+                child: Text(
+                  'SIGN UP',
+                  style: TextStyle(color: Colors.white),
                 ),
-              ],
-            ),
+              ),
+              Center(
+                  child: TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/');
+                      },
+                      child: Text(
+                        'Back To Login Page',
+                        style: TextStyle(color: Colors.blue),
+                      ))),
+            ],
           ),
         ),
-      );
-    }
+      ),
+    );
   }
+}
